f91a561746d41d0228b9eade586d3c2549182117
Nunez <yn2295@cumc.columbia.edu>
2021-10-18 14:43:13 -0400

    fixing lag bug and re-running all analysis

--- code/models/m_00_load_datasets.R
+++ code/models/m_00_load_datasets.R
@@ -1,36 +1,32 @@
 
+
+
 # 1 year exposure averages
 
-data_1yr <- read_csv(paste0(yanelli_datasets, "one_yr_avg_data.csv")) %>%
+data_1yr <- read_csv(paste0(yanelli_datasets, "one_yr_avg_data_ses.csv")) %>%
   mutate(family_SES = as.factor(family_SES),
          place_birth = as.factor(place_birth),
          residence = as.factor(residence),
-         civst = as.factor(civst)) %>% 
-  dplyr::filter(residence != "Greenland") %>%
-  droplevels() 
+         civst = as.factor(civst))
 summary(data_1yr)
 
 # 5 years exposure averages
 
-data_5yrs <- read_csv(paste0(yanelli_datasets, "five_yr_avg_data.csv")) %>%
+data_5yrs <- read_csv(paste0(yanelli_datasets, "five_yr_avg_data_ses.csv")) %>%
   mutate(family_SES = as.factor(family_SES),
          place_birth = as.factor(place_birth),
          residence = as.factor(residence),
-         civst = as.factor(civst)) %>% 
-  dplyr::filter(residence != "Greenland") %>%
-  droplevels()
+         civst = as.factor(civst)) 
 
 summary(data_5yrs)
 
 # 10 years exposure averages
 
-data_10yrs <- read_csv(paste0(yanelli_datasets, "ten_yrs_avg_data.csv")) %>%
+data_10yrs <- read_csv(paste0(yanelli_datasets, "ten_yrs_avg_data_ses.csv")) %>%
   mutate(family_SES = as.factor(family_SES),
          place_birth = as.factor(place_birth),
          residence = as.factor(residence),
-         civst = as.factor(civst)) %>% 
-  dplyr::filter(residence != "Greenland") %>%
-  droplevels()
+         civst = as.factor(civst)) 
 summary(data_10yrs)
 
 # 10 years lag averages 
@@ -39,6 +35,6 @@ data_lags <- read_csv(paste0(yanelli_datasets, "lagged_10yrs_parishSES.csv")) %>
   mutate(family_SES = as.factor(family_SES),
          place_birth = as.factor(place_birth),
          residence = as.factor(residence),
-         civst = as.factor(civst)) %>% 
-  dplyr::filter(residence != "Greenland") %>% # only has 22 observations which is too little and results in unstable estimates so I removed this category 
-  droplevels()
+         civst = as.factor(civst)) 
+
+
